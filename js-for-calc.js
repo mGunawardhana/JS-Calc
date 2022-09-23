f60158ac69f09ae3b576047ed2e45e1e303c1e78
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-09-23 23:49:16 +0530

    if type any btn displaying its value set up

--- js-for-calc.js
+++ js-for-calc.js
@@ -4,4 +4,19 @@
  *   ~  * what's app - 071 - 9043372
  */
 
+(function (){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function (button){
+        //checking which event is working ?
+        button.addEventListener('click',function (e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+
+})();
 
